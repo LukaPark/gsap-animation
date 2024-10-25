59e6f6711373942f859e98a9a6c139f6ddcb0f34
LukaPark <solchan0106@ronfic.com>
2024-10-25 16:55:05 +0900

    선수 설명 및 카드 앞면 UI 작성

--- src/CardAnimation.tsx
+++ src/CardAnimation.tsx
@@ -2,6 +2,55 @@ import {useRef} from "react";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 
+const playerData: PlayerInfo[] = [
+    {
+        name: 'Stephen Curry',
+        team_name: 'Golden State Warriors',
+        position: 'G',
+        player_number: 30,
+        team_color: 'linear-gradient(135deg, #1D428A 0%, #FDB927 100%)',
+        player_achievement: ['3x NBA Champion', '2x NBA MVP'],
+    },
+    {
+        name: 'LeBron James',
+        team_name: 'Los Angeles Lakers',
+        position: 'F',
+        player_number: 23,
+        team_color: 'linear-gradient(135deg, #552583 0%, #FFC72C 100%)',
+        player_achievement: ['4x NBA Champion', '4x NBA MVP'],
+    },
+    {
+        name: 'Kevin Durant',
+        team_name: 'Phoenix Suns',
+        position: 'F',
+        player_number: 35,
+        team_color: 'linear-gradient(135deg, #1D1160 0%, #E56020 100%)',
+        player_achievement: ['2x NBA Champion', '2x NBA MVP'],
+    },
+    {
+        name: 'Luka Dončić',
+        team_name: 'Dallas Mavericks',
+        position: 'G',
+        player_number: 77,
+        team_color: 'linear-gradient(135deg, #00538C 0%, #002B5C 100%)',
+        player_achievement: ['Rookie of the Year', '2x NBA All-Star'],
+    },
+    {
+        name: 'Michael Jordan',
+        team_name: 'Chicago Bulls',
+        position: 'G',
+        player_number: 23,
+        team_color: 'linear-gradient(135deg, #CE1141 0%, #000000 100%)',
+        player_achievement: ['6x NBA Champion', '5x NBA MVP'],
+    }
+]
+
+const card = {
+    w: 300,
+    h: 420,
+    gap: 20,
+}
+
 export default function CardAnimation() {
     const scope = useRef<HTMLDivElement>(null);
     const {contextSafe} = useGSAP(() => {
@@ -9,8 +58,8 @@ export default function CardAnimation() {
 
     const openCards = contextSafe(() => {
         const combine = gsap.timeline();
-        const w = 48 * 4;
-        const gap = 5 * 4;
+        const w = card.w;
+        const gap = card.gap;
 
         // 카드 중앙으로 모으기
         combine.to('.card', {
@@ -96,12 +145,14 @@ export default function CardAnimation() {
     return (
         <div ref={scope} className={'bg-gray-950 w-screen h-screen'}>
             <Flash />
-            <div className={'w-full flex gap-5 justify-center items-center min-h-[50vh] p-5'}>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+            <div className={'w-full flex justify-center items-center min-h-[50vh] p-5'} style={{
+                gap: `${card.gap}px`,
+            }}>
+                {
+                    playerData.map((player, index) => (
+                        <Card key={index} player={player}/>
+                    ))
+                }
             </div>
             <div className={'flex justify-center w-full gap-5'}>
                 <button onClick={openCards} className={'bg-gray-950 text-white'}>확인하기</button>
@@ -111,12 +162,15 @@ export default function CardAnimation() {
     );
 }
 
-function Card() {
+function Card({player}: { player: PlayerInfo }) {
     return (
         <div
-            className={'relative card w-48 h-64 bg-[#FFFDD0] rounded-[18px] shadow-2xl flex justify-center items-center'}>
+            className={`relative card bg-[#FFFDD0] rounded-[18px] shadow-2xl flex justify-center items-center`} style={{
+                width: `${card.w}px`,
+                height: `${card.h}px`,
+        }}>
             <Cover />
-            <CardFront />
+            <CardFront player={player} />
             <CardBack />
         </div>
     );
@@ -134,14 +188,58 @@ const Flash = () => {
     );
 };
 
-const CardFront = () => {
+type PlayerInfo = {
+    name: string;
+    team_name: string;
+    team_color: string;
+    position: string;
+    player_number: number;
+    player_achievement: string[];
+}
+
+
+const CardFront = ({player}: { player: PlayerInfo }) => {
     return (
-        <div className="card-front absolute w-full h-full" style={{
+        <div className="card-front bg-amber-200 absolute w-full h-full rounded-[15px] overflow-hidden" style={{
             backfaceVisibility: 'hidden',
             transform: 'rotateY(180deg)',
         }}>
-            {/* 앞면 내용 */}
-            앞면
+            <div className={'w-full h-3/5'}>
+                {/*<img src="https://www.nba.com/assets/logos/teams/primary/web/ATL.svg" alt="ATL"/>*/}
+                <div className={'w-full h-full bg-amber-200'}></div>
+            </div>
+            <div className={'card-content rounded-[15px] p-[15px] text-white text-center h-2/5'} style={{
+                background: player.team_color,
+            }}>
+                <div
+                    className={'text-[24px] font-bold my-[10px]'}
+                    style={{
+                        textShadow: '1px 1px 3px rgba(0,0,0,0.3)',
+                    }}
+                >
+                    {player.name}
+                </div>
+                <div
+                    className={'text-[18px]'}
+                    style={{
+                        textShadow: '1px 1px 3px rgba(0,0,0,0.2)',
+                    }}
+                >
+                    {player.team_name}
+                </div>
+                <div
+                    className={'text-[16px] opacity-90'}
+                >
+                    #{player.player_number} | {player.position}
+                </div>
+                <div
+                    className={'flex justify-between text-[14px] my-[10px] opacity-90 p-[5px] rounded-[5px] bg-[rgba(255,255,255,0.1)]'}
+                >
+                    {player.player_achievement.map((achievement, index) => (
+                        <span key={index}>{achievement}</span>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
@@ -150,25 +248,26 @@ const CardBack = () => {
     return (
         <div className={'card-back w-full h-full'} style={{
             backfaceVisibility: 'hidden',
+            // transform: 'rotateY(180deg)',
         }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 256" width={192} height={256}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${card.w} ${card.h}`} width={card.w} height={card.h}>
                 {/* 카드 배경 */}
-                <rect width="192" height="256" rx="15" fill="#333333"/>
+                <rect width={card.w} height={card.h} rx="15" fill="#333333"/>
 
                 {/* 장식적인 테두리 */}
-                <rect x="10" y="10" width="172" height="236" rx="10"
+                <rect x="10" y="10" width={card.w-20} height={card.h - 20} rx="15"
                       fill="none" stroke="#666666" stroke-width="2"/>
 
                 {/* NBA 로고 스타일의 텍스트 */}
-                <text x="96" y="138"
-                      font-family="Arial" font-weight="bold" font-size="36"
+                <text x={(card.w / 2)} y={card.h /2 + 24}
+                      font-family="Arial" font-weight="bold" font-size="72"
                       fill="#FFFFFF" text-anchor="middle">
                     NBA
                 </text>
 
                 {/* 장식적인 선들 */}
-                <path d="M20 80 L172 80" stroke="#666666" stroke-width="2"/>
-                <path d="M20 170 L172 170" stroke="#666666" stroke-width="2"/>
+                <path d={`M20 ${card.h/4} L${card.w-20} ${card.h/4}`} stroke="#666666" stroke-width="2"/>
+                <path d={`M20 ${card.h/4*3} L${card.w-20} ${card.h/4*3}`} stroke="#666666" stroke-width="2"/>
             </svg>
         </div>
     );
